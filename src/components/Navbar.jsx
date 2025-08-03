ae406e29dc171c44b2153c7184c2678426426c95
Jasvinder Singh <heyjasvinder@gmail.com>
2025-08-04 00:49:31 +0530

    Initial commit - SpendSense Personal Finance Tracker App

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ToggleTheme from "./ToggleTheme";
 import Profile from "./Profile";
 import { Link } from "react-router-dom";
+import spendsenseLogo from '../assets/spendsense-logo.png'
+import spendsenseDarkLogo from '../assets/spendsense-logo-dark.png'
 
 const Navbar = () => {
   return (
    <header className="w-full px-4 py-2 flex flex-col sm:flex-row justify-between items-center border-b bg-white dark:bg-gray-900 dark:border-gray-800 shadow-sm gap-2">
   <Link className="flex justify-center sm:justify-start">
-    <img src="/src/assets/spendsense-logo.png" alt="SpendSense Logo" className="h-13 block dark:hidden" />
-    <img src="/src/assets/spendsense-logo-dark.png" alt="SpendSense Logo" className="h-13 hidden dark:block" />
+    <img src={spendsenseLogo} alt="SpendSense Logo" className="h-13 block dark:hidden" />
+    <img src={spendsenseDarkLogo} alt="SpendSense Logo" className="h-13 hidden dark:block" />
   </Link>
 
   <div className="flex gap-2">
